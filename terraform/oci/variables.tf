172ace48c7fafc280684eeee23e9b9950e279206
Tanjim Hossain <tangimhossain1@gmail.com>
2022-04-28 22:06:46 +0600

    test oci as module

---
+++ terraform/oci/variables.tf
@@ -0,0 +1,8 @@
+variable "compartment_ocid" { type = string }
+variable "tenancy_ocid" { type = string }
+variable "user_ocid" { type = string }
+variable "fingerprint" { type = string}
+variable "region" { type = string } 
+variable "private_key" { type = string }
+variable "private_key_password" { type = string }
+
