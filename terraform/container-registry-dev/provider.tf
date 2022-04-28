d5691f69dc259396bbe7ed980b3e2b6fad37dbf4
Tanjim Hossain <tangimhossain1@gmail.com>
2022-04-29 00:39:51 +0600

    test oci

--- terraform/container-registry-dev/provider.tf
+++ terraform/container-registry-dev/provider.tf
@@ -1,4 +1,5 @@
-variable "private_key" { type = string }
+variable "oci_private_key" { type = string }
+variable "oci_private_key_password" { type = string }
 variable "compartment_ocid" { type = string }
 
 terraform {
@@ -12,5 +13,6 @@ terraform {
 
 // NOTE: couldn't use TF_VAR_private_key in terraform cloud
 provider "oci" {
-  private_key          = var.private_key
+  private_key          = var.oci_private_key
+  private_key_password = var.oci_private_key_password
 }
