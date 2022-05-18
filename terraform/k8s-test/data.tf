21b5126a6002cea45a9c3aacfab35321991d07d6
Tanjim Hossain <tangimhossain1@gmail.com>
2022-05-18 09:08:51 +0600

    add private network

--- terraform/k8s-test/data.tf
+++ terraform/k8s-test/data.tf
@@ -37,17 +37,6 @@ data "template_cloudinit_config" "k3s_server" {
   }
 }
 
-data "template_cloudinit_config" "external_db" {
-  gzip          = true
-  base64_encode = true
-
-  part {
-    filename     = "externaldb.yaml"
-    content_type = "text/cloud-config"
-    content      = data.template_file.external_db_cloud_init_file.rendered
-  }
-}
-
 data "template_file" "k3s_server_template" {
   template = file("scripts/k3s_server.template.sh")
 
@@ -64,20 +53,3 @@ data "template_file" "k3s_server_cloud_init_file" {
   }
 }
 
-
-data "template_file" "external_db_template" {
-  template = file("scripts/external_db.template.sh")
-
-  vars = {
-    password = random_password.sqlpassword.result
-  }
-}
-
-data "template_file" "external_db_cloud_init_file" {
-  template = file("cloud-init/cloud-init.template.yaml")
-
-  vars = {
-    bootstrap_sh_content = base64gzip(data.template_file.external_db_template.rendered)
-  }
-
-}
