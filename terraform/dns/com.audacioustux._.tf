958b62adbf70f6024cd61cdeee80127ecfee84d5
Tanjim Hossain <tangimhossain1@gmail.com>
2023-04-29 16:31:50 +0600

    Update com.audacioustux._.tf

--- terraform/dns/com.audacioustux._.tf
+++ terraform/dns/com.audacioustux._.tf
@@ -41,62 +41,6 @@ resource "cloudflare_record" "com_audacioustux_A_argocd-test" {
   proxied = true
 }
 
-resource "cloudflare_record" "craftkori_A" {
-  zone_id = local.zone_audacioustux.id
-  name    = "craftkori"
-  value   = var.craftkori_ip_addr
-  type    = "A"
-  proxied = true
-}
-
-resource "cloudflare_record" "craftkori_SRV_mc_tcp_4120" {
-  zone_id = local.zone_audacioustux.id
-  name    = "_minecraft._tcp"
-  type    = "SRV"
-
-  data {
-    service  = "_minecraft"
-    proto    = "_tcp"
-    name     = "craftkori"
-    priority = 0
-    weight   = 0
-    port     = 4120
-    target   = "craftkori.audacioustux.com"
-  }
-}
-
-resource "cloudflare_record" "craftkori_SRV_mc_tcp_5968" {
-  zone_id = local.zone_audacioustux.id
-  name    = "_minecraft._tcp"
-  type    = "SRV"
-
-  data {
-    service  = "_minecraft"
-    proto    = "_tcp"
-    name     = "craftkori"
-    priority = 0
-    weight   = 0
-    port     = 5968
-    target   = "craftkori.audacioustux.com"
-  }
-}
-
-resource "cloudflare_record" "craftkori_SRV_mc_udp_24454" {
-  zone_id = local.zone_audacioustux.id
-  name    = "_minecraft._udp"
-  type    = "SRV"
-
-  data {
-    service  = "_minecraft"
-    proto    = "_udp"
-    name     = "craftkori"
-    priority = 0
-    weight   = 0
-    port     = 24454
-    target   = "craftkori.audacioustux.com"
-  }
-}
-
 resource "cloudflare_record" "audacioustux_com_TXT_dmarc" {
   zone_id = local.zone_audacioustux.id
   name    = "_dmarc"
